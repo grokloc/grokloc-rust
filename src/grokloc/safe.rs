7271b43febe29dc1dc9a161f063b0760a488f222
Brad Clawsie <brad@b7j0c.org>
2022-07-03 18:55:51 -0500

    safe string tests

--- src/grokloc/safe.rs
+++ src/grokloc/safe.rs
@@ -1,5 +1,6 @@
 //! safe provides value filtering functions
 
+use regex::Regex;
 use std::time;
 
 pub const STR_MAX: usize = 8192;
@@ -7,21 +8,46 @@ pub const STR_MAX: usize = 8192;
 #[allow(dead_code)]
 /// string_is makes sure strings are realtively safe for db use
 fn string_is(s: &str) -> bool {
-    s.contains('"')
+    let re_insert = Regex::new(r"insert\s+into").unwrap();
+    let re_table = Regex::new(r"(?:drop|create)\s+table").unwrap();
+    let re_query = Regex::new(r"(?:select|update)\s+").unwrap();
+    let lc_s = s.to_lowercase();
+    !(s.contains('"')
         || s.contains('\'')
         || s.contains('>')
         || s.contains('<')
         || s.contains('`')
-        || s.contains("&lt;")
-        || s.contains("&gt;")
+        || lc_s.contains("&lt;")
+        || lc_s.contains("&gt;")
+        || re_insert.is_match(&lc_s)
+        || re_table.is_match(&lc_s)
+        || re_query.is_match(&lc_s)
         || s.len() > STR_MAX
+        || s.is_empty())
 }
 
 #[allow(dead_code)]
 /// unixtime_is determines if a value is too far in the future and likely in error
 fn unixtime_is(t: &time::SystemTime) -> bool {
     t.duration_since(time::SystemTime::UNIX_EPOCH)
-        .expect("hi")
+        .expect("duration epoch failure")
         .as_secs()
         < 1767139200
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn string_is_test() {
+        assert!(!string_is(""));
+        assert!(!string_is("hello ' there"));
+        assert!(!string_is("hello > there"));
+        assert!(!string_is("hello < there"));
+        assert!(!string_is("select col from table"));
+        assert!(!string_is("update col"));
+        assert!(!string_is("hello drop table"));
+        assert!(!string_is("hello create table"));
+    }
 }
